a158dd1d37dc5c8f7ece5586e96d2ef4ca6683dd
vayoa <ew0nd3r123@gmail.com>
2022-10-21 01:06:18 +0300

    fix(in-app-updates): Fixed github workflow 49 (😡😾😔😢).

--- lib/blocs/update/app_version.dart
+++ lib/blocs/update/app_version.dart
@@ -16,6 +16,7 @@ class AppVersion extends Comparable<AppVersion> with Compared<AppVersion> {
     }
   }
 
+  /// Creates a temporary empty version.
   AppVersion.empty() {
     version = Version.none;
     releaseNotes = null;
